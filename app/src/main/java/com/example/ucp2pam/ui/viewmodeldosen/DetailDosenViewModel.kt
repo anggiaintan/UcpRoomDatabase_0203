2d8b304b6fa2a4e81cb03cee8739eeee3cc92de9
Anggia Intan <intananggia23@gmail.com>
2024-12-20 23:16:55 +0700

    membuat class DetailDosenViewModel

--- app/src/main/java/com/example/ucp2pam/ui/viewmodeldosen/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodeldosen/DetailDosenViewModel.kt
@@ -1,6 +1,56 @@
 package com.example.ucp2pam.ui.viewmodeldosen
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2pam.data.entity.Dosen
+import com.example.ucp2pam.repository.RepositoryDosen
 import com.example.ucp2pam.ui.viewmodeldosen.DosenViewModel.DosenEvent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class DetailDosenViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDosen: RepositoryDosen,
+) : ViewModel() {
+    private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDetailDosen.NIDN])
+
+    val detailUiState: StateFlow<DetailDosenUiState> = repositoryDosen.getDosen(_nidn)
+        .filterNotNull()
+        .map {
+            DetailDosenUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailDosenUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailDosenUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailDosenUiState(isLoading = true)
+        )
+
+    fun deleteDosen() {
+        detailUiState.value.detailUiEvent.toDosenEntity().let {
+            viewModelScope.launch {
+                repositoryDosen.deleteDosen(it)
+            }
+        }
+    }
+}
 
 data class DetailDosenUiState(
     val detailUiEvent: DosenEvent = DosenEvent(),
