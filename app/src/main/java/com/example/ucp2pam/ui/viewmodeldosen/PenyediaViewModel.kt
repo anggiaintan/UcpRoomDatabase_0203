a31347d0fff54f0fa58b21087d9b7e6f8f4b9f9c
Anggia Intan <intananggia23@gmail.com>
2024-12-21 21:29:52 +0700

    membuat object bernama PenyediaViewModel sebagai penyedia ViewModel Factory

--- app/src/main/java/com/example/ucp2pam/ui/viewmodeldosen/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodeldosen/PenyediaViewModel.kt
@@ -1,7 +1,39 @@
 package com.example.ucp2pam.ui.viewmodeldosen
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2pam.DosenApp
+import com.example.ucp2pam.ui.viewmodelmk.MataKuliahViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                DosenApp().containerApp.repositoryDosen
+            )
+        }
+        initializer {
+            HomeDosenViewModel(
+                DosenApp().containerApp.repositoryDosen
+            )
+        }
+        initializer {
+            DetailDosenViewModel(
+                createSavedStateHandle(),
+                DosenApp().containerApp.repositoryDosen
+            )
+        }
+        initializer {
+            MataKuliahViewModel(
+                DosenApp().containerApp.RepositoryMataKuliah,
+                DosenApp().containerApp.RepositoryDosen
+            )
+        }
+    }
+}
 
 fun CreationExtras.sisforApp(): SisforApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as SisforApp)
