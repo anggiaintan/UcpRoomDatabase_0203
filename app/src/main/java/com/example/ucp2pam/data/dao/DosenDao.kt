072b34f25365539ccee598f86fc947007ca4c134
Anggia Intan <intananggia23@gmail.com>
2024-12-22 22:08:40 +0700

    mengubah fitur untuk mendukung pengambilan data menggunakan Flow dan menambahkan fungsi untuk mengambil data dosen berdasarkan nidn

--- app/src/main/java/com/example/ucp2pam/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/DosenDao.kt
@@ -4,12 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.ucp2pam.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DosenDao {
     @Insert
     suspend fun insertDosen(dosen: Dosen)
 
-    @Query("SELECT * FROM Dosen")
-    suspend fun getAllDosen(): List<Dosen>
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getDosen(nidn: String) : Flow<Dosen>
 }
