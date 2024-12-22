fbfbe6bafc096ac49c68181fdee11f4ce22b04a7
Anggia Intan <intananggia23@gmail.com>
2024-12-23 02:05:51 +0700

    membuat function untuk Halaman Utama

---
+++ app/src/main/java/com/example/ucp2pam/ui/view/HlmUtama.kt
@@ -0,0 +1,183 @@
+package com.example.ucp2pam.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.ucp2pam.R
+
+@Composable
+fun HalamanUtama(navController: NavController, modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .background(color = colorResource(id = R.color.purple_700))
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(30.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier.size(110.dp),
+                        painter = painterResource(id = R.drawable.umyhd),
+                        contentDescription = ""
+                    )
+                    Spacer(modifier = Modifier.padding(start = 20.dp))
+                    Column {
+                        Text(
+                            text = "Universitas Muhammadiyah Yogyakarta",
+                            color = Color.White,
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "Teknologi Informasi",
+                            color = Color.White,
+                            fontWeight = FontWeight.Light
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.umyhd),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(450.dp)
+                            .align(Alignment.Center)
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(
+                                color = Color.White.copy(alpha = 0.9f),
+                                shape = RoundedCornerShape(
+                                    topEnd = 15.dp,
+                                    topStart = 15.dp
+                                )
+                            )
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            Text(
+                                text = "Aplikasi KRS",
+                                color = Color.Black,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+
+                            Button(
+                                onClick = {
+                                    navController.navigate("Dosen")
+                                },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF003f5c),
+                                    contentColor = Color.White
+                                ),
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .height(50.dp)
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(12.dp)
+                            ) {
+                                Row(
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        text = "Dosen",
+                                        fontSize = 18.sp,
+                                        modifier = Modifier.weight(1f),
+                                        textAlign = TextAlign.Start
+                                    )
+                                    Icon(
+                                        imageVector = Icons.Default.KeyboardArrowRight,
+                                        contentDescription = "Arrow End",
+                                        tint = Color.White
+                                    )
+                                }
+                            }
+
+                            Button(
+                                onClick = {
+                                    navController.navigate("MK")
+                                },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF003f5c),
+                                    contentColor = Color.White
+                                ),
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .height(50.dp)
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(12.dp)
+                            ) {
+                                Row(
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        text = "Matakuliah",
+                                        fontSize = 18.sp,
+                                        modifier = Modifier.weight(1f),
+                                        textAlign = TextAlign.Start
+                                    )
+                                    Icon(
+                                        imageVector = Icons.Default.KeyboardArrowRight,
+                                        contentDescription = "Arrow End",
+                                        tint = Color.White
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
+}
