8f2406cc60c16b44400189ee42e4b5d490e6dfcb
Anggia Intan <intananggia23@gmail.com>
2024-12-22 22:20:46 +0700

    membuat companion object

--- app/src/main/java/com/example/ucp2pam/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/ucp2pam/data/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2pam.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2pam.data.dao.DosenDao
 import com.example.ucp2pam.data.dao.MataKuliahDao
@@ -11,4 +13,18 @@ import com.example.ucp2pam.data.entity.MataKuliah
 abstract class AppDatabase : RoomDatabase() {
     abstract fun dosenDao(): DosenDao
     abstract fun mataKuliahDao(): MataKuliahDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE:AppDatabase? =null
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "AppDatabase"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
 }
