640bcfd70201c953f604d029f4ac0deab6320b97
Anggia Intan <intananggia23@gmail.com>
2024-12-23 02:11:01 +0700

    menambahkan import ContainerApp

--- app/src/main/java/com/example/ucp2pam/DosenApp.kt
+++ app/src/main/java/com/example/ucp2pam/DosenApp.kt
@@ -1,8 +1,9 @@
 package com.example.ucp2pam
 
 import android.app.Application
+import com.example.ucp2pam.dependeciesinjection.ContainerApp
 
-class DosenApp : Application {
+class DosenApp : Application() {
     lateinit var containerApp: ContainerApp
     override fun onCreate() {
         super.onCreate()
