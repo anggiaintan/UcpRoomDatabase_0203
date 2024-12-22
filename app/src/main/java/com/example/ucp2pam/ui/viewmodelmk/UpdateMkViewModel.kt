fb9c5914af4c23299c9e0559cea661d13a7798ad
Anggia Intan <intananggia23@gmail.com>
2024-12-22 14:15:41 +0700

    membuat class UpdateMkViewModel

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodelmk/UpdateMkViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.ucp2pam.ui.viewmodelmk
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.repository.RepositoryDosen
+import com.example.ucp2pam.repository.RepositoryMataKuliah
+import com.example.ucp2pam.ui.viewmodelmk.MataKuliahViewModel.MataKuliahUIState
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMkViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMataKuliah: RepositoryMataKuliah,
+    private val repositoryDosen: RepositoryDosen
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(MataKuliahUIState())
+        private set
+
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiUpdateMatkul.KODE])
+
+    var dosenList by mutableStateOf(listOf<String>())
+
+    // Ambil daftar dosen dari repository
+    fun getDosenList() {
+        viewModelScope.launch {
+            repositoryDosen.getAllDosen().collect { dosenEntities ->
+                dosenList = dosenEntities.map { it.nama }
+            }
+        }
+    }
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMataKuliah.getMataKuliah(_kode)
+                .filterNotNull()
+                .first()
+                .toUIStateMatkul()
+            getDosenList()
+        }
+    }
