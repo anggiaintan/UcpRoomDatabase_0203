5460b8f921de81a4a8b7dcd4dfb444c1739bd9f0
Anggia Intan <intananggia23@gmail.com>
2024-12-23 01:06:36 +0700

    membuat function BodyHomeMatkulView

--- app/src/main/java/com/example/ucp2pam/ui/view/matkul/HomeMatkulView.kt
+++ app/src/main/java/com/example/ucp2pam/ui/view/matkul/HomeMatkulView.kt
@@ -1,29 +1,151 @@
 package com.example.ucp2pam.ui.view.matkul
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2pam.R
 import com.example.ucp2pam.data.entity.MataKuliah
+import com.example.ucp2pam.ui.viewmodelmk.HomeMatkulUiState
+import kotlinx.coroutines.launch
+
+@Composable
+fun BodyHomeMatkulView(
+    homeUiState: HomeMatkulUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier.fillMaxSize()
+        .background(
+            color = colorResource(
+                id = R.color.purple_200
+            )
+        )){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier.size(110.dp),
+                painter = painterResource(id = R.drawable.umyhd),
+                contentDescription = "Logo UMY"
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Teknologi Informasi",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = colorResource(
+                        id = R.color.teal_700
+                    ),
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+        ){
+            val coroutineScope = rememberCoroutineScope()
+            val snackbarHostState = remember { SnackbarHostState() }
+            when{
+                homeUiState.isLoading ->{
+                    // Menampilkan indikator loading
+                    Box(
+                        modifier = modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        CircularProgressIndicator()
+                    }
+                }
+                homeUiState.isError -> {
+                    // Menampilkan pesan error
+                    LaunchedEffect(homeUiState) {
+                        homeUiState.errorMessage?.let { message ->
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar(message)
+                            }
+                        }
+                    }
+                }
+                homeUiState.listMatkul.isEmpty() ->{
+                    // Menampilkan pesan jika data kosong
+                    Box(
+                        modifier = modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = "Tidak ada data mata kuliah.",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                }
+                else -> {
+                    // Menampilkan daftar mata kuliah
+                    ListMatkul(
+                        listMatkul = homeUiState.listMatkul,
+                        onClick = {
+                            onClick(it)
+                            println(it)
+                        },
+                        modifier = modifier
+                    )
+                }
+            }
+        }
+    }
+
+}
 
 @Composable
 fun ListMatkul(
