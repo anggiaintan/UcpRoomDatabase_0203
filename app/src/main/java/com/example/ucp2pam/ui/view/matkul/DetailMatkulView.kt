0d4463b5126bc60875d8d3455def18e13f3012ab
Anggia Intan <intananggia23@gmail.com>
2024-12-23 01:23:41 +0700

    membuat private fun DeleteConfirmationDialogMatkul

---
+++ app/src/main/java/com/example/ucp2pam/ui/view/matkul/DetailMatkulView.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2pam.ui.view.matkul
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialogMatkul(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { /* Do nothing */ },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah Anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Tidak")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Ya")
+            }
+        }
+    )
+}
